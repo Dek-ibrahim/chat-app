c41139a24821e6547959cfbcf87912792ee90a2c
Dek ibrahim muse <122024928+Dek-ibrahim@users.noreply.github.com>
2023-05-29 17:24:09 +0300

    oh wow :(
    😎😎😎

---
+++ chat app/app.js
@@ -0,0 +1,10 @@
+let h1 = document.createElement('h1')
+let body = document.querySelector('body')
+
+body.appendChild(h1)
+
+h1.textContent = "Hello World Header 1"
+
+let h1Style = ``
+
+h1.style.cssText = h1Style
